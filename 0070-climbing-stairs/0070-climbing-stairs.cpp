6d350f7cc2e62b1751637d515c6917d758645dda
amangit6174 <6174amanverma@gmail.com>
2026-08-03 03:01:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.55%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map<int,int>memo;
+    int climbStairs(int n) {
+        if(memo.count(n)){
+            return memo[n];
+        }
+        if(n==0){
+            return 0;
+        }if(n==1){
+            return 1;
+        }if(n==2){
+            return 2;
+        }
+        int result = climbStairs(n-1)+climbStairs(n-2);
+        memo[n]=result;
+        return result;
+    }
+};
